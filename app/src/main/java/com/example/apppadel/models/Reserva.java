321c5b4f6bcca8082dfc54ee13cd4e6488fce7f0
Alberto <albertoguillenmartin4@gmail.com>
2024-05-13 22:03:23 +0200

    Botón de Eliminar Reservas funcionado con éxito

--- app/src/main/java/com/example/apppadel/models/Reserva.java
+++ app/src/main/java/com/example/apppadel/models/Reserva.java
@@ -16,51 +16,39 @@ public class Reserva {
     public String getIdReserva() {
         return idReserva;
     }
-
     public void setIdReserva(String idReserva) {
         this.idReserva = idReserva;
     }
-
     public String getIdUser() {
         return idUser;
     }
-
     public void setIdUser(String idUser) {
         this.idUser = idUser;
     }
-
     public String getIdPista() {
         return idPista;
     }
-
     public void setIdPista(String idPista) {
         this.idPista = idPista;
     }
-
     public String getFechaReserva() {
         return fechaReserva;
     }
-
     public void setFechaReserva(String fechaReserva) {
         this.fechaReserva = fechaReserva;
     }
-
     public String getHoraInicioReserva() {
         return horaInicioReserva;
     }
-
     public void setHoraInicioReserva(String horaInicioReserva) {
         this.horaInicioReserva = horaInicioReserva;
     }
-
     public String getHoraFinReserva() {
         return horaFinReserva;
     }
-
     public void setHoraFinReserva(String horaFinReserva) {
         this.horaFinReserva = horaFinReserva;
     }
-
     @Override
     public String toString() {
         return horaInicioReserva;
